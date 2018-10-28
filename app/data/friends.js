d71a855dc72f412637276c5755a655f6460f59e7
ANS NEOphyte <jpbrown1023@gmail.com>
2018-10-28 13:19:47 -0400

    updated server.js, api/html routes, and friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+var friendsArray = [
+    {
+      userName: "SpongeBob",
+      photoLink: "https://upload.wikimedia.org/wikipedia/en/thumb/4/47/Spongebob-squarepants.svg/330px-Spongebob-squarepants.svg.png",
+      answers: [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      }
+  ];
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = friendsArray;
